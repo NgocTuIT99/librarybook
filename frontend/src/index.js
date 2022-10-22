aa7feb2d6ee534a477028651abddf200a0587da0
NgocTuIT99 <ngoctuvo493@gmail.com>
2022-10-22 19:10:28 +0700

    fix 7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import "../node_modules/bootstrap/dist/css/bootstrap-login-form.min.css";
-import "../node_modules/bootstrap/dist/js/mdb.min.js";
+import "../assets/css/bootstrap-login-form.min.css";
+import "../assets/js/mdb.min.js";
 import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
